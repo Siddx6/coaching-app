3d796810363d143c6f07f1fcf990a8c148734558
Siddharth Kumar <siddharthkumar2344@gmail.com>
2026-07-24 14:12:04 +0530

    Add Attendance page: batch/date selection, per-student present/absent marking

--- frontend/src/pages/StudentDetail.jsx
+++ frontend/src/pages/StudentDetail.jsx
@@ -22,6 +22,7 @@ function StudentDetail() {
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
     loadData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
